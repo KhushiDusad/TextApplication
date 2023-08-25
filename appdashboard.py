8050c3ad6d0f6a56a7cd49baeab194a6f2aeafd1
Khushi Dusad <106913778+KhushiDusad@users.noreply.github.com>
2023-08-25 10:53:01 +0530

    Update appdashboard.py

--- appdashboard.py
+++ appdashboard.py
@@ -1,26 +1,20 @@
 import streamlit as st
 import pdftotextconverter
 import texttopdfconverter
-import texttospeechconverter
-import image_text_extractor
 
 st.set_page_config(layout="wide")
 
 def main():
-    st.title("Textopia: All About Text")
+    st.title("PDF to/from Text")
 
     # Sidebar with buttons for each application
     st.sidebar.title("Select an Application")
-    app_selection = st.sidebar.radio("Go to:", ("PDF to Text Converter", "Text to PDF Converter", "Text to Speech Converter", "Image Text Extractor"))
+    app_selection = st.sidebar.radio("Go to:", ("PDF to Text Converter", "Text to PDF Converter"))
 
     if app_selection == "PDF to Text Converter":
         pdftotextconverter.main()
     elif app_selection == "Text to PDF Converter":
         texttopdfconverter.main()
-    elif app_selection == "Text to Speech Converter":
-        texttospeechconverter.main()
-    elif app_selection == "Image Text Extractor":
-        image_text_extractor.main()
 
 if __name__ == "__main__":
     main()
